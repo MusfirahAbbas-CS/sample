1d8eecd542255d8966228bca82d51d7abd1955a9
Musfirah Abbas <2024CS181@student.uet.edu.pk>
2025-05-05 17:28:23 +0500

    main.css
    smaple

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using App.BL;
+using App.DL;
+using App.UI;
+namespace App
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int logo;
+            
+            int option;
+
+            
+            
+            do
+            {
+                Console.Clear();
+                logo = UserUI.logoPrint();
+                option = UserUI.menu();
+                UserBL user = new UserBL();
+                if (option==2)
+                {
+                    
+                    user = UserUI.AddUser();
+                    if(UserDL.add(user))
+                    {
+                        UserUI.SignupSuccess();
+                    }
+                }
+                if(option==1)
+                {
+                    user=UserUI.SignIn();
+                    if(UserBL.SignupRole(user)=="Admin")
+                    {
+                        Console.Clear();
+                        int adminchoice=UserUI.AdminMenu();
+                        if(adminchoice==1)
+                        {
+                            int regOption=UserUI.Regmenu();
+                            if(regOption==1)
+                            {
+                                string output = UserUI.AddStudent();
+                                Console.Clear();
+                            }
+
+                            
+                        }
+                    }
+                }
+            }
+            while (option < 3);
+        }
+    }
+}
